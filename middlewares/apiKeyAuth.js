b17af0d0bfa54ee0e9a794d54356c57152dbfbe8
Kareem Taiye <olayinkahassan117@gmail.com>
2026-03-05 15:37:51 +0000

    created a develop branch

--- middlewares/apiKeyAuth.js
+++ middlewares/apiKeyAuth.js
@@ -2,5 +2,7 @@ import MerchantService from "../services/merchantService.js";
 import AppError from "../utilities/AppError.js";
 
 export default class ApiKeyAuth {
-  static async verify(req, res, next) {}
+  static async verify(req, res, next) {
+    //Some comments
+  }
 }
