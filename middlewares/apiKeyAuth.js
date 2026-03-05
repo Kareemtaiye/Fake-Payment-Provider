25cd31c751505cf311f1767749fbf0996fb3b311
Kareem Taiye <olayinkahassan117@gmail.com>
2026-03-05 15:32:09 +0000

    changing the project to a mini scale event-driven project

--- middlewares/apiKeyAuth.js
+++ middlewares/apiKeyAuth.js
@@ -2,7 +2,5 @@ import MerchantService from "../services/merchantService.js";
 import AppError from "../utilities/AppError.js";
 
 export default class ApiKeyAuth {
-  static async verify(req, res, next) {
-    let;
-  }
+  static async verify(req, res, next) {}
 }
