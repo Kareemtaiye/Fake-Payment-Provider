ef13b99ce946c556d922b49a5d5bac2895f89f19
Kareem Taiye <olayinkahassan117@gmail.com>
2026-03-19 10:16:54 +0000

    docker fully functioning and my machine freee

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import pool from "./config/db.js";
 const { PORT, DOCKER_PORT, NODE_ENV } = process.env;
 
 const server = app.listen(DOCKER_PORT, () => {
-  console.log(`Fake PP server running ${NODE_ENV} on port: ${DOCKER_PORT}`);
+  console.log(`Fake PP server running ${NODE_ENV} on port: ${PORT}`);
 });
 
 // process.on("SIGINT")
